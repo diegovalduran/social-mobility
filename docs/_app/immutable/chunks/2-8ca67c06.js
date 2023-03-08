f0e43aedec15260533088427c6166ef5ca9c13a7
russellsamora <russellsamora@gmail.com>
2023-03-08 10:05:15 -0500

    update github pages

---
+++ docs/_app/immutable/chunks/2-8ca67c06.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-273e67c3.js";export{t as component};
--- docs/_app/immutable/chunks/5-df7b65f6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/method/_page.svelte-a057fb23.js";export{t as component};
